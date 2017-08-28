97deecc81e999205cd372286b17b6d5de46030f8
bararan <bararan>
2017-08-28 11:39:30 -0400

    re-added morgan for github

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const express = require("express")
       , MongoStore = require("connect-mongo")(session)
       , path = require("path")
       , pug = require("pug")
-    //   , morgan = require("morgan")
+      , morgan = require("morgan")
       , passport = require("passport")
       , TwitterStrategy = require("passport-twitter").Strategy
       , flash = require("connect-flash")
@@ -44,7 +44,7 @@ dbClient.connect(url, (err, db) => {
     app.set("port", (process.env.PORT || 5000));
     app.use(
         express.static(path.join(__dirname, "static"))
-        // , morgan("error")
+        , morgan("error")
         , bodyParser.json()
         , bodyParser.urlencoded({extended: true})
         
